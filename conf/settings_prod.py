51fdc5952c4feb9379f0da9385951abdd0dd74d7
Serg Lichachev <aughingl@gmail.com>
2020-11-19 15:44:10 +1000

    some settings cahnged
    Signed-off-by: Serg Lichachev <aughingl@gmail.com>

--- conf/settings_prod.py
+++ conf/settings_prod.py
@@ -2,10 +2,10 @@
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'vladzdrav_mos',
-        'USER': 'vladzdrav',
-        'PASSWORD': 'Gtu92E15rvl',
-        'HOST': 'postgresql-vladzdrav.alwaysdata.net'
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': ''
     }
 }
 
